cbc34dd84e3e643d8c0b245f5cb0040cc9a8e0e9
Cam Courtman-Stock <cam.courtman-stock@sainsburys.co.uk>
2022-05-25 13:45:01 +0100

    feat: add kafka consumer

---
+++ src/main/java/com/cam090/kafka/kafkaConsumer/listener/KafkaConsumer.java
@@ -0,0 +1,22 @@
+package com.cam090.kafka.kafkaConsumer.listener;
+
+
+import com.cam090.kafka.kafkaConsumer.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", containerGroup = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", containerGroup = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
---
+++ src/test/java/com/cam090/kafka/kafkaConsumer/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cam090.kafka.kafkaConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
